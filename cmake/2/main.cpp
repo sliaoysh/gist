39c49f104dc502b0ceabb70f7004c9eb85b5aac1
Sam <sam@example.com>
2021-06-13 15:53:06 +0800

    add cmake

---
+++ cmake/2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+//int main(int argc, const char*[])
+{
+  vector<int> v;
+
+  cout << "ok" << endl;
+
+  return 0;
+  //return 1;
+}
